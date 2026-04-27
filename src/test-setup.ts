1ba757a816965c0a1ec26032922a1c67b8ea813f
RSI Platform <platform@rsi2.digify.no>
2026-04-27 08:03:43 +0200

    fix: Add visualViewport polyfill to test-setup.ts

--- src/test-setup.ts
+++ src/test-setup.ts
@@ -4,3 +4,21 @@ global.ResizeObserver = class ResizeObserver {
   unobserve() {}
   disconnect() {}
 }
+
+// Polyfill visualViewport for jsdom (used by Vuetify overlay positioning)
+if (!window.visualViewport) {
+  Object.defineProperty(window, 'visualViewport', {
+    value: {
+      width: 1024,
+      height: 768,
+      offsetLeft: 0,
+      offsetTop: 0,
+      pageLeft: 0,
+      pageTop: 0,
+      scale: 1,
+      addEventListener: () => {},
+      removeEventListener: () => {},
+    },
+    writable: true,
+  })
+}
